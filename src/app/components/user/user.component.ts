ed731084b38b35bce6e2af507e46fa355690a481
maxime pierre <mpierre@pulseinc.com>
2017-07-17 10:57:57 -0400

    adding 'edit mode' on profile page

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -16,25 +16,45 @@ import { User } from '../../shared/custom-types/classes/user';
 })
 export class UserComponent implements OnInit {
 
-  private formTitle: string = 'edit info';
+  private formTitle: string = 'Profile Info';
 
-  private user: User = new User();
+  private editMode: boolean = false;
+
+  private admin: boolean = false;
+
+  private user: User;
+
+  private isLoading: boolean = false;
 
   constructor(private webService: WebService,
-              private errorDisplayService: ErrorDisplayService) {}
+              private errorDisplayService: ErrorDisplayService) {
+    this.isLoading = true;
+  }
 
   public ngOnInit(): void {
     this.webService.getUser().subscribe((res: User) => {
       this.user = res;
+
+      this.isLoading = false;
+      this.admin = (this.user.role !== undefined && this.user.role === 'admin');
     });
   }
 
   public post(): void {
     if (this.user.firstname.trim().length > 0 && this.user.lastname.trim().length > 0) {
-      this.webService.saveUser(this.user).subscribe();
+      this.webService.saveUser(this.user).subscribe(() => {
+        this.editMode = false;
+        this.formTitle = 'Profile Info';
+      });
     } else {
       this.errorDisplayService.display('Some entries are invalid');
     }
+  }
+
+  public toggleEditMode(): void {
+    this.editMode = !this.editMode;
+
+    this.formTitle = (this.editMode) ? 'Edit Profile Info' : 'Profile Info';
   }
 
 }
