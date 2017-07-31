e02d8df36d0e1b4584638af32727c7f0f5a53d91
maxime pierre <mpierre@pulseinc.com>
2017-07-31 16:24:12 -0400

    login form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 // Services
 import { AuthService } from '../../shared/services/auth.service';
+import { ErrorDisplayService } from '../../shared/services/error-display.service';
 
 // Classes
 import { Credentials } from '../../shared/custom-types/classes/credentials';
@@ -19,8 +22,14 @@ export class LoginComponent implements OnInit {
 
   private credentials = new Credentials();
 
+  private loginForm: FormGroup;
+
+  private formSubmitted: boolean;
+
   constructor(private authService: AuthService,
-              private router: Router) {}
+              private router: Router,
+              private fb: FormBuilder,
+              private errorDisplayService: ErrorDisplayService) {}
 
   public ngOnInit(): void {
     this.credentials.account = (localStorage.getItem('account') !== null) ?
@@ -29,10 +38,31 @@ export class LoginComponent implements OnInit {
     if (this.authService.isAuthenticated) {
       this.router.navigate(['/dashboard']);
     }
+
+    this.loginForm = this.fb.group({
+      account: [this.credentials.account, Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.formSubmitted = false;
   }
 
-  public login(): void {
-    this.authService.login(this.credentials);
+  public resetForm(): void {
+    this.formSubmitted = false;
+  }
+
+  public login(creds: Credentials): void {
+    this.formSubmitted = true;
+
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value);
+    } else {
+      this.errorDisplayService.display(
+        'Your authentication failed. Please verify ' +
+        'that your account, user id and password are correct.'
+      );
+    }
   }
 
 }
